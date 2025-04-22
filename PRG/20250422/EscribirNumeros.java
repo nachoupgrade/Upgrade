d6bd98132f4ce1255d2cf80e264c1fdf20e3740b
Nacho Muñoz <ignacio.munoz@upgrade-hub.com>
2025-04-22 18:30:33 +0200

    Added 22_04_2025 session files

---
+++ PRG/20250422/EscribirNumeros.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class EscribirNumeros {
+    public static void main(String[] args) {
+        String fichero = "PRG\\20250422\\numeros.txt";
+
+        try (PrintWriter pw = new PrintWriter(new File(fichero))) {
+            for (int i = 1; i < 1000; i++) {
+                pw.println(i);
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Problemas al abrir el fichero: " + e.getMessage());
+        }
+    }
+}
