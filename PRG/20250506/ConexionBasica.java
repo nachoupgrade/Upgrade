497c3b45f7c96e6b649c6e8fe3c2e084da8d15fd
Nacho Muñoz <ignacio.munoz@upgrade-hub.com>
2025-05-06 18:33:18 +0200

    Added 2025_05_06 session files. Added jdbc files to connect with mysql and sqlite databases

---
+++ PRG/20250506/ConexionBasica.java
@@ -0,0 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBasica {
+    public static void main(String[] args) {
+        try {
+            // Indicar el tipo y el lugar de la base de datos
+            String urlbdd = "jdbc:sqlite:C:\\Users\\Nacho\\AppData\\Roaming\\DBeaverData\\workspace6\\.metadata\\sample-database-sqlite-1\\Chinook.db";
+
+            // Usuario de la base de datos con el que vamos a realizar el login
+            String usr = "root";
+
+            // Contraseña para ese usuario en esa base de datos
+            String pwd = "1234";
+
+            // Establece la conexión
+            Connection con = DriverManager.getConnection(urlbdd, usr, pwd);
+            System.out.println("Conexión con la BDD realizada con éxito.");
+
+            // Cerrar la conexión
+            con.close();
+
+        } catch (SQLException e) {
+            System.out.println("No se ha podido establecer la conexión con la BDD.");
+            e.printStackTrace();
+        }
+    }
+}
